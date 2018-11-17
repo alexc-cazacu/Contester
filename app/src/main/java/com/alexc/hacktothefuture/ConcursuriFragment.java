849c39c085bddf816041090ae7bbf1662ebb0605
xfde <alex.constantin432@gmail.com>
2018-11-17 15:44:31 +0200

    Almost ready

--- app/src/main/java/com/alexc/hacktothefuture/ConcursuriFragment.java
+++ app/src/main/java/com/alexc/hacktothefuture/ConcursuriFragment.java
@@ -6,27 +6,75 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class ConcursuriFragment extends Fragment {
+import java.nio.BufferUnderflowException;
+
+public class ConcursuriFragment extends Fragment implements View.OnClickListener {
 
     Button concursuri,olimpiade,altele;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_concursuri, null);
         concursuri= (Button)view.findViewById(R.id.buttonConcursuri);
+        concursuri.setOnClickListener(this);
         altele=view.findViewById(R.id.buttonAltele);
+        altele.setOnClickListener(this);
         olimpiade=view.findViewById(R.id.buttonOlimpiade);
-        olimpiade.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                //loadFragment(new lists());
-            }
-        });
+        olimpiade.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.buttonAltele:
+
+                Fragment fr = new lists("altele");
+                Bundle bundle = new Bundle();
+                bundle.putString("Key","altele");
+                fr.setArguments(bundle);
+                FragmentManager fm =getFragmentManager();
+                FragmentTransaction fragmentTransaction = fm.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, fr);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                break;
+
+            case R.id.buttonConcursuri:
+
+                Fragment fr1 = new lists("concursuri");
+                Bundle bundle1=new Bundle();
+                bundle1.putString("Key","concursuri");
+                fr1.setArguments(bundle1);
+                FragmentManager fm1 = getFragmentManager();
+                FragmentTransaction fragmentTransaction1 = fm1.beginTransaction();
+                fragmentTransaction1.replace(R.id.fragment_container, fr1);
+                fragmentTransaction1.addToBackStack(null);
+                fragmentTransaction1.commit();
+                break;
+
+            case R.id.buttonOlimpiade:
+
+
+                Fragment fr2=new lists("olimpiade");
+                Bundle bundle2=new Bundle();
+                bundle2.putString("Key","Olimpiade");
+                fr2.setArguments(bundle2);
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container,fr2).addToBackStack(null).commit();
+
+                break;
+
+        }
     }
 
 }
--- app/src/main/java/com/alexc/hacktothefuture/HomeFragment.java
+++ app/src/main/java/com/alexc/hacktothefuture/HomeFragment.java
@@ -25,8 +25,8 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_home, null);
-        int images[]={R.drawable.slide1,R.drawable.slide2};
-        String texts[]={"24 Nov la 09:00 _ 25 Nov la 19:00\n\n2NHack se adresează pasionaților de Machine Learning si Neural Networks. Indiferent de limbajul favorit, sunteți invitați să contribuiți la soluții practice, bazate pe ML/NN.","8 Dec la 09:00 _ 9 Dec la 10:00\n\nPregateste-ti echipa si vino la Hackathon.Vei intampina 99 de probleme, dintre care sigur nu dataset-urile."};
+        int images[]={R.drawable.slide1,R.drawable.slide2, R.drawable.slide3};
+        String texts[]={"24 Nov la 09:00 _ 25 Nov la 19:00\n\n\n2NHack se adresează pasionaților de Machine Learning si Neural Networks. Indiferent de limbajul favorit, sunteți invitați să contribuiți la soluții practice, bazate pe ML/NN.","8 Dec la 09:00 _ 9 Dec la 10:00\n\n\nPregateste-ti echipa si vino la Hackathon.Vei intampina 99 de probleme, dintre care sigur nu dataset-urile.","Astăzi la 10:00 - 15:00\n\n\nIn ultimii 30 de ani tehnologia WEB a fost revolutionata de mai multe ori. Cea mai importanta evolutie este dezvoltarea Inteligentei Artificiale. Acesta a ajuns in punctul de a putea crea o diferenta in scietatea noasta.Vino la Hackathon si alfa mai multe"};
         v_flipper=(ViewFlipper) view.findViewById(R.id.v_flipper);
         t_flipper=(ViewFlipper) view.findViewById(R.id.t_flipper);
         SpannableString ss= new SpannableString(texts[0]);
@@ -44,7 +44,7 @@ public class HomeFragment extends Fragment {
         TextView textView=new TextView(getContext());
         textView.setText(text);
         t_flipper.addView(textView);
-        t_flipper.setFlipInterval(10000);
+        t_flipper.setFlipInterval(3500);
         t_flipper.setAutoStart(true);
     }
     public void flipperImages(int image)
@@ -52,7 +52,7 @@ public class HomeFragment extends Fragment {
         ImageView imageView=new ImageView(getContext());
         imageView.setBackgroundResource(image);
         v_flipper.addView(imageView);
-        v_flipper.setFlipInterval(10000);
+        v_flipper.setFlipInterval(3500);
         v_flipper.setAutoStart(true);
 
         v_flipper.setInAnimation(getContext(),android.R.anim.slide_in_left);
